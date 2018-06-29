fcaebc2eaa1e15c564caebc16fa5f919a7c712f3
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-06-29 16:39:46 -0500

    starting the questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -19,14 +19,18 @@ $(document).ready(function () {
         }
     }
 
-    var triviaQuestions = ["Which is the only member of Pink Floyd to play on all of their albums?", "Second Question that's really long?"];
-    var triviaAnswers = ["this is an answer, this is an answer, and this is an answer", "these are answers too, these as well, yep these too"]
+    var triviaQuestions = ["Which is the only member of Pink Floyd to play on all of their albums?", "What is Syd Barretts real name?", "Which are the three animals featured on Pink Floyd's 'Animals' album?", "Who was the original lead guitarist for Pink Floyd?",
+        "What song off Pink Floyd's first album didn't have any lyrics?", "When was Roger Waters born?", "Who uttered the only lyrics for Pink Floyd's 'One of These Days'?"
+    ];
+    var triviaAnswers = ["this is an answer", "this is an answer", "and this is an answer", "these are answers too", "these as well, yep these too"]
 
-    function questions () {
-        for (i = 0; i < triviaQuestions.length && triviaAnswers.length; i++){
-            $(".questions").append("<ol>" + triviaQuestions[i] + "</br>" + triviaAnswers[i]);
+    function questions() {
+        for (i = 0; i < triviaQuestions.length && triviaAnswers.length; i++) {
+            $(".questions").append(("<ul>" + triviaQuestions[i]) + "</br>" + ("<input type='radio'>" + triviaAnswers[i]));
         }
     }
+
+
     //add in trivia questions and buttons
 
     //add in if questions are right or wrong.  This may be able to be done with one function
