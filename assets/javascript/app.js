698db76474978d0e275435ca55f3f6a2cae90a5e
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-06-29 16:08:33 -0500

    making progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,38 @@
 $(document).ready(function () {
+
+
     var timeLeft = 30;
     var countTime = document.getElementById("countdown");
-    // var timerID = setInterval(countDown, 1000);
 
     document.getElementById("start").onclick = function () {
         countDown();
-        console.log(timeLeft);
         setInterval(countDown, 1000);
-       
+        questions();
     }
 
     function countDown() {
         if (timeLeft === 0) {
-            
+
         } else {
             countTime.innerHTML = ("<h3>Time Remaining: " + timeLeft + "</h3");
             timeLeft--;
         }
     }
-   
+
+    var triviaQuestions = ["Which is the only member of Pink Floyd to play on all of their albums?", "Second Question that's really long?"];
+    var triviaAnswers = ["this is an answer, this is an answer, and this is an answer", "these are answers too, these as well, yep these too"]
+
+    function questions () {
+        for (i = 0; i < triviaQuestions.length && triviaAnswers.length; i++){
+            $(".questions").append("<ol>" + triviaQuestions[i] + "</br>" + triviaAnswers[i]);
+        }
+    }
+    //add in trivia questions and buttons
+
+    //add in if questions are right or wrong.  This may be able to be done with one function
 
 
- ;
+
 
 
 
